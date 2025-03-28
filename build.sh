f9fd801f5c1df13dabd16bd7693dece2063c64af
deltasampler <deltasampler@gmail.com>
2025-03-28 22:13:48 +0200

    initial commit

---
+++ build.sh
@@ -0,0 +1,4 @@
+clear
+mkdir -p out/
+g++ -Wall -Wextra -I modules/ -o out/test.out source/main.cpp
+out/test.out
---
+++ source/main.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "types.h"
+
+s32 main() {
+    printf("Hello, World!\n");
+
+    return 0;
+}
